17c05a236fd22fe5c878034e0e527c7d1617dca2
Luka Žugaj <luka.zugaj7@gmail.com>
2021-03-11 15:56:12 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/travelchecker/TravelCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.travelchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelCheckerApplication.class, args);
+    }
+
+}
